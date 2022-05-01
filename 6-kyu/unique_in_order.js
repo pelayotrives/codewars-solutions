0e9aed4ab65cd70c3685e7c9811bbe8989aeb564
Pelayo Trives <pelayotrives@gmail.com>
2022-05-02 01:11:18 +0200

    New 6 kyu file.

--- 6-kyu/unique_in_order.js
+++ 6-kyu/unique_in_order.js
@@ -1,4 +1,4 @@
-// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+// Implement the function uniqueInOrder which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
 
 //? Example:
 //? uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
